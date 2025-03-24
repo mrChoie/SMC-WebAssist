a4b8da5b392f63401fcea84fb3dca4440668ed87
mrchoiRyzen7 <drazilletoledo@gmail.com>
2025-03-24 10:53:09 +0800

    Refactor user interface elements for better visibility and organization; streamline home route rendering and clean up cookie handling in user login

--- controller/server.js
+++ controller/server.js
@@ -12,10 +12,14 @@ import db from './ticketHandler.js';
 const app = express();
 // app.engine("html", ejs.renderFile);
 // app.set('view engine', 'html');
+
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+// app.use('/',(req, res) => {
+//     res.render('home.ejs')
+// })
 app.use('/checkCookie', checkCookie);
 app.use('/getTickets', db);
 app.use('/getInfo', user);
