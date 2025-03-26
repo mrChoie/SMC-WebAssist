388b8fab185eb9ed6ff3dc21d443985321914a56
mrchoiRyzen7 <drazilletoledo@gmail.com>
2025-03-26 20:54:24 +0800

    - Enhance ticket display - improve UI layout and styling for better user experience - added links to a ticket card

--- public/js/account.js
+++ public/js/account.js
@@ -52,28 +52,44 @@ function displayTickets(tickets) {
     // console.log(tickets)
     for (x = 0; x<tickets.numOfTkts; x++) {
 
-        var div= document.createElement("div");
+        var rowDiv= document.createElement("div");
+        var colDivContent= document.createElement("div");
+        var colDivLink= document.createElement("div");
+        var colDivLink= document.createElement("div");
         var title=document.createElement("p");
         var id=document.createElement("p");
         var content=document.createElement("p");
         var date=document.createElement("p");
+        var link=document.createElement("a");
+        var btn=document.createElement("i");
 
-        div.classList.add("container","border","border-dark","rounded","mb-3","animate");
+        rowDiv.classList.add("row","bg-light","animate");
+        colDivContent.classList.add("col-10","ticketContent");
+        colDivLink.classList.add("col-2","ticketLink","text-center");
         title.classList.add("ptitle")
         id.classList.add("pid")
         content.classList.add("pcontent")
         date.classList.add("pdate")
+        btn.classList.add("fa-solid","fa-circle-chevron-right")
+        link.href = "/smc-webassist/view-ticket/ticket?id="+tickets.tickets[x].tktID;
+        
 
         title.textContent = tickets.tickets[x].tktSubj;
         id.textContent = "Ticket ID: "+ tickets.tickets[x].tktID;
-        content.textContent = tickets.tickets[x].tktDesc.substring(0, 200) + ". . .";
+        content.textContent = tickets.tickets[x].tktDesc.substring(0, 170) + ". . .";
         date.textContent = tickets.tickets[x].tktTimestamp;
 
-        ticketDiv.appendChild(div)
-        div.appendChild(title)
-        div.appendChild(id)
-        div.appendChild(content)
-        div.appendChild(date)
+        ticketDiv.appendChild(rowDiv)
+
+        rowDiv.appendChild(colDivContent)
+        colDivContent.appendChild(title)
+        colDivContent.appendChild(id)
+        colDivContent.appendChild(content)
+        colDivContent.appendChild(date)
+
+        rowDiv.appendChild(colDivLink)
+        colDivLink.appendChild(link)
+        link.appendChild(btn)
     }
 }
 
