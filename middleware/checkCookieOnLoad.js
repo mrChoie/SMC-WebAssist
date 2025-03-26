c0318a41c4fa84a301ccb884c9b6a8580a29a588
mrchoiRyzen7 <drazilletoledo@gmail.com>
2025-03-26 20:54:24 +0800

    - Enhance ticket display - improve UI layout and styling for better user experience - added links to a ticket card

--- middleware/checkCookieOnLoad.js
+++ middleware/checkCookieOnLoad.js
@@ -10,19 +10,44 @@ const checkCookie = async (req, res, next) => {
     const cookies = Object.fromEntries(req.headers.cookie?.split('; ').map(c => c.split('=')) || []);
     const token = cookies;
     const lvl = cookies.lvl;
-    // console.log("Token:", lvl)
-    // 20 = not logged in, display sign-in div
-    // 21 = logged in, display profile div
+    // console.log("JWT Token:\n ", token.token)
+
     if (cookies.token==null){
         res.json({token, message: "User is not logged in", statusCode:20 });
         // next(json({message: "User is not logged in", statusCode:'20'}))
         // res.json({token})
     } else {
         res.json({cookies, lvl, message: "User is logged in", statusCode:21 });
-        // next(json({message: "User is logged in", statusCode:'21'}))
-        // res.json({token})
-        
     }
+
+    // var validate
+
+    // const lvl = cookies.lvl;
+    // // console.log("Token:", lvl)
+    // // 20 = not logged in, display sign-in div
+    // // 21 = logged in, display profile div
+    // // console.log("Tokens:\n", token)
+
+    // if (cookies.token==null){
+    //     res.json({token, message: "User is not logged in", statusCode:20 });
+    //     // next(json({message: "User is not logged in", statusCode:'20'}))
+    //     // res.json({token})
+    // } else {
+    //     validate = jwt.verify(token.token, process.env.ACCESS_TOKEN_SECRET)
+    // }
+
+    
+    // if (validate){
+    //     console.log("validated= ",validate)
+    //     res.json({cookies, lvl, message: "User is logged in", statusCode:21 });
+    // } else {
+    //     res.json({token, message: "User is not logged in", statusCode:20 });
+    //     // next(json({message: "User is not logged in", statusCode:'20'}))
+    //     // res.json({token})
+    // }
+
+
+
     // try {
     //     if (token == null) return next().res.json({statusCode:'20'})
     //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
